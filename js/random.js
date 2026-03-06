f9d27457371a5cb1b062deb8acc54091807ed55b
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-06 18:42:14 -0300

    feat: implement random number generation
    - Add random number generator within defined interval
    - Support generation with or without repetition
    - Integrate validations checks
    - Display success toast after drawing numbers

---
+++ js/random.js
@@ -0,0 +1,52 @@
+import { showToastMessage } from './toast.js';
+import {
+  isMaxGreaterThanMin,
+  isNumberAlreadyDrawn,
+  isQuantityGreaterThanInterval,
+} from './validation.js';
+
+const SUCCESS_ICON = '../assets/success.png';
+
+export function generateRandomNumbers(quantity, minNumber, maxNumber, shouldNotRepeat) {
+  if (!isMaxGreaterThanMin(minNumber, maxNumber)) return;
+
+  const result = shouldNotRepeat
+    ? generateUniqueRandomNumbers(quantity, minNumber, maxNumber)
+    : generateRandomNumbersAllowingRepetition(quantity, minNumber, maxNumber);
+
+  if (result) showToastMessage('Número sorteado com sucesso!', SUCCESS_ICON);
+
+  return result;
+}
+
+function generateRandomNumbersAllowingRepetition(quantity, minNumber, maxNumber) {
+  const result = [];
+
+  for (let i = 0; i < quantity; i++) {
+    result.push(generateRandomNumber(minNumber, maxNumber));
+  }
+
+  return result;
+}
+
+function generateUniqueRandomNumbers(quantity, minNumber, maxNumber) {
+  const result = [];
+
+  if (!isQuantityGreaterThanInterval(quantity, minNumber, maxNumber)) {
+    while (result.length < quantity) {
+      const randomNumber = generateRandomNumber(minNumber, maxNumber);
+
+      if (!isNumberAlreadyDrawn(result, randomNumber)) {
+        result.push(randomNumber);
+      }
+    }
+
+    return result;
+  }
+
+  return false;
+}
+
+function generateRandomNumber(minNumber, maxNumber) {
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+}
